9ff37e731d8be971900e3a8b9810f6ec6c18f9cc
doraemonaaaa <3478428491@qq.com>
2026-01-07 00:13:38 +0800

    s

--- agentflow/tools/sam_perception/examples/example.py
+++ agentflow/tools/sam_perception/examples/example.py
@@ -2,7 +2,7 @@ import os
 import sys
 import json
 
-from agentflow.agentflow.tools.sam_perception.tool import SAM2_Perception_Tool
+from agentflow.agents.tools.sam_perception.tool import SAM2_Perception_Tool
 
 # 初始化SAM2工具（可选模型：sam2.1_hiera_tiny, sam2.1_hiera_small, sam2.1_hiera_base_plus, sam2.1_hiera_large）
 tool = SAM2_Perception_Tool(
